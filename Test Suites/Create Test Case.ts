4ca5efa616477d4d5aee6581a86bbf1ccee45543
Minh Thong <thongnmtran@kms-technology.com>
2019-12-09 16:54:42 +0700

    add refresh test case scripts

---
+++ Test Suites/Create Test Case.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Create Test Case</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>180</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6653e8e1-ebbf-4a5e-807f-dd6efd298265</testSuiteGuid>
+   <testCaseLink>
+      <guid>24c15429-3c04-4578-90dc-7c8e47ff6f2b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common/Application/Start application</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d0c309e2-e4a4-4d9c-bb7a-45818b3ec947</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common/Project/Wait for project to load</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>890fc753-bde5-4054-9a90-2c87218e3b1e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Artifacts/Test Cases/Create/Create Test Case by using File menu</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>530f7cd6-4d00-4926-8728-6c3ee164864a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Artifacts/Test Cases/Create/Create Test Case by using context menu</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a9f0b349-a251-41a7-b47e-a918c9f7f560</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common/Application/Close application</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
